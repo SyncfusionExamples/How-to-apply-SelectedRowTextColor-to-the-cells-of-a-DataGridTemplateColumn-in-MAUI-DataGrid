7bc180f4561db29001104b9b4a6f70c16e883385
Tamilarasan Paranthaman <tamilarasan.paranthaman@syncfusion.com>
2023-11-09 17:53:22 +0530

    Initial commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿
+using Syncfusion.Maui.DataGrid;
+
+namespace MauiApp1
+{
+
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            dataGrid.CellRenderers.Remove("Template");
+            dataGrid.CellRenderers.Add("Template", new CustomDataGridCellTemplateRenderer());
+
+        }
+    }
+
+    public class CustomDataGridCellTemplateRenderer : DataGridCellTemplateRenderer
+    {
+        protected override void OnSetCellStyle(DataColumnBase dataColumn)
+        {
+            base.OnSetCellStyle(dataColumn);
+
+            if (dataColumn?.DataGridColumn != null)
+            {
+                var gridStyle = this.DataGrid?.DefaultStyle;
+                DataGridCell gridCell = dataColumn.ColumnElement as DataGridCell;
+
+                if (this.DataGrid.SelectedRows.Any(row => row == dataColumn.RowData))
+                {
+                    gridCell.TextColor = gridStyle?.SelectedRowTextColor;
+                    Label sfLabel = dataColumn.ColumnElement?.Content as Label;
+                    if (sfLabel != null)
+                    {
+                        sfLabel.TextColor = gridStyle?.SelectedRowTextColor;
+                    }
+                }
+                else
+                {
+                    gridCell.TextColor = gridStyle?.RowTextColor;
+                    Label sfLabel = dataColumn.ColumnElement?.Content as Label;
+                    if (sfLabel != null)
+                    {
+                        sfLabel.TextColor = gridStyle?.RowTextColor;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
